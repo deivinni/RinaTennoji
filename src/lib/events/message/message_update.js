e97be5c608b9e67ab4e54ad01ba9bb541af77b17
DEIVINNI <41592653+DEIVINNI@users.noreply.github.com>
2019-10-27 21:32:40 -0300

    Delete message_update.js

--- src/lib/events/message/message_update.js
+++
@@ -1,7 +0,0 @@
-const { prefix } = require('../../../util/config');
-
-module.exports = (bot, _, newMsg) => {
-  if (newMsg.content.startsWith(prefix)) {
-    return bot.emit('message', newMsg);
-  } else return;
-}
